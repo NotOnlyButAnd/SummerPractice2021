2fe37121980d65d46b411ffc9b6b4fec6a9cfcff
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-05-25 20:06:53 +0300

    Euler Method v25.05.21
    находит решение верно, но чтобы строить график нормальный (не только на отрезке [0,1]) надо искать ассимптоты вертикальные, и потом делать фокусы с количеством точек вблизи этих ассимптот.... Но в требуемом отрезке все тип-топ)

---
+++ SummerPractice2021Csharp/SummerPractice2021Csharp/TaskCauchy.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace SummerPractice2021Csharp
+{
+    partial class TaskCauchy
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.DrawGraphics = new System.Windows.Forms.Button();
+            this.MaxNevyazka = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Аналитическое решение";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Решение методом Эйлера";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1349, 590);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "Графики решений";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(265, 617);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(746, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Рассматриваем задачу Коши: y\' * cosx + y * sinx + 3 * y^2 * cosx = 0, y(0) = 1. П" +
+    "ри х принадлежащем отрезку [0;1]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(265, 646);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите количество отрезков (N):";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBoxN.Location = new System.Drawing.Point(506, 643);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(120, 23);
+            this.textBoxN.TabIndex = 3;
+            // 
+            // DrawGraphics
+            // 
+            this.DrawGraphics.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.DrawGraphics.Location = new System.Drawing.Point(660, 641);
+            this.DrawGraphics.Name = "DrawGraphics";
+            this.DrawGraphics.Size = new System.Drawing.Size(154, 27);
+            this.DrawGraphics.TabIndex = 4;
+            this.DrawGraphics.Text = "Построить графики!";
+            this.DrawGraphics.UseVisualStyleBackColor = true;
+            this.DrawGraphics.Click += new System.EventHandler(this.DrawGraphics_Click);
+            // 
+            // MaxNevyazka
+            // 
+            this.MaxNevyazka.AutoSize = true;
+            this.MaxNevyazka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.MaxNevyazka.Location = new System.Drawing.Point(897, 646);
+            this.MaxNevyazka.Name = "MaxNevyazka";
+            this.MaxNevyazka.Size = new System.Drawing.Size(0, 17);
+            this.MaxNevyazka.TabIndex = 5;
+            // 
+            // TaskCauchy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1373, 679);
+            this.Controls.Add(this.MaxNevyazka);
+            this.Controls.Add(this.DrawGraphics);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.MaximumSize = new System.Drawing.Size(1389, 718);
+            this.MinimumSize = new System.Drawing.Size(1389, 718);
+            this.Name = "TaskCauchy";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button DrawGraphics;
+        private System.Windows.Forms.Label MaxNevyazka;
+    }
+}
+
---
+++ SummerPractice2021Csharp/SummerPractice2021Csharp/TaskCauchy.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SummerPractice2021Csharp
+{
+    public partial class TaskCauchy : Form
+    {
+        public TaskCauchy()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawGraphics_Click(object sender, EventArgs e)
+        {
+            int N;
+            if (Int32.TryParse(textBoxN.Text, out N))   // проверка введенного N на корректность
+            {
+                // очищаем график и лэйбл со значением макс невязки
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                MaxNevyazka.Text = "";
+
+                // задаем все начальные значения, что нам даны в условии задачи
+                double x0 = 0, y0 = 1, xN = 1, step = (xN - x0) / N;
+
+                // выставляем границы отображения оси абсцисс на графике [0;1]
+                chart1.ChartAreas[0].AxisX.Minimum = x0;
+                chart1.ChartAreas[0].AxisX.Maximum = xN;
+
+                // считаем массивы Х и У решения методом Эйлера
+                EulerMethod task = new EulerMethod(x0, y0, step);
+                task.Euler(N);
+
+                // рисуем график полученного методом Эйлера решения 
+                chart1.Series[1].Points.DataBindXY(task.X, task.Y);
+
+                // строим график аналитически полученной функции
+                double[] xFunc = new double[N + 1];
+                double[] yFunc = new double[N + 1];
+                xFunc = task.X;
+                for (int i = 0; i <= N; i++)
+                    yFunc[i] = AnalyticsFunc(xFunc[i]);
+                chart1.Series[0].Points.DataBindXY(xFunc, yFunc);
+                maximalNev(yFunc, task.Y, N);
+            }
+            else
+            {
+                MessageBox.Show("Некорректный ввод!!!");
+                MaxNevyazka.Text = "";
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+            }
+
+        }
+
+        // вывод массива double в messageBox
+        /*
+        public void outArrToMBox(double[] arr)
+        {
+            string str = "";
+            for (int i = 0; i < arr.Length; i++)
+                str += Convert.ToString(arr[i]) + " ";
+            MessageBox.Show(str);
+        }
+        */
+
+        // функция, полученная при аналитическом решении
+        public double AnalyticsFunc (double pointX)
+        {
+            return Math.Cos(pointX) / (3 * Math.Sin(pointX) + 1);
+        }
+
+        // максимальная невязка
+        public void maximalNev (double[] AnalyticY, double[] EulerY, int N)
+        {
+            double maxNev = Math.Abs(AnalyticY[0] - EulerY[0]);
+            for (int i = 1; i < N + 1; i++)
+            {
+                double t = Math.Abs(AnalyticY[i] - EulerY[i]);
+                if (t > maxNev)
+                    maxNev = t;
+            }
+            MaxNevyazka.Text = " Максимальная невязка: " + Convert.ToString(maxNev);
+        }
+    }
+
+    public class EulerMethod
+    {
+        public double X0 { get; private set; }  // начальные условия
+        public double Y0 { get; private set; }  // начальные условия
+        public double Step { get; private set; }    // шаг (h)
+        public double[] X { get; private set; }     // итоговый массив иксов 
+        public double[] Y { get; private set; }     // итоговый массив игреков
+
+        public void Euler(int N)
+        {
+            X = new double[N+1];
+            Y = new double[N+1];
+
+            X[0] = X0;
+            Y[0] = Y0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                X[i] = X[i - 1] + Step;
+                Y[i] = Y[i - 1] + Step * DUFunc(X[i - 1], Y[i - 1]);
+            }
+        }
+
+        // функция из исходного уравнения dy/dx = f(x,y)
+        public double DUFunc(double pointX, double pointY)
+        {
+            return -((pointY * Math.Sin(pointX) + 3 * pointY * pointY * Math.Cos(pointX)) / Math.Cos(pointX));
+        }
+
+        // конструктор
+        public EulerMethod(double x0, double y0, double step)
+        {
+            this.X0 = x0;
+            this.Y0 = y0;
+            this.Step = step;
+        }
+    }
+}
+
